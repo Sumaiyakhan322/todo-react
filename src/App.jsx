3de9d6002d5c0d0ee539e99c26981ebb30921954
Sumaiyakhan322 <sumaiya.khan32665@gmail.com>
2024-06-12 20:38:31 +0600

    added the basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+  return <>
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  <div className="text-center mx-20">
+
+  <h1 className="text-6xl my-10 font-bold">Todo App</h1>
+
+
+  {/* Todo add */}
+   <div className="grid grid-cols-5 gap-10">
+     <div className="col-span-2">
+        <input type="text" name="" id="" className="border border-black w-full" />
+     </div>
+     <div className="col-span-2">
+       <input type="date" className="w-full" />
+     </div>
+     <div>
+      <button>Add</button>
+     </div>
+   </div>
+  </div>
+
+  </>;
 }
 
-export default App
+export default App;
